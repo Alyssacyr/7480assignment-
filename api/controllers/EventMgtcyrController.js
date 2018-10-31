c1cfa2ddf82361dd624d5abdbe80242a853028e9
EventMgtcyr <bryan@Bryans-MacBook-Pro.local>
2018-11-01 03:18:32 +0800

    Initial commit

---
+++ api/controllers/EventMgtcyrController.js
@@ -0,0 +1,36 @@
+/**
+ * EventMgtController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+    index: async function (req, res) {
+
+        return res.view('pages/homepage.ejs');
+
+    },
+    search: async function (req, res) {
+
+        return res.view('pages/search.ejs');
+
+    },
+    create: async function (req, res) {
+
+        return res.view('pages/create.ejs');
+
+    },
+    detail: async function (req, res) {
+
+        return res.view('pages/detail.ejs');
+
+    },
+    admin: async function (req, res) {
+
+        return res.view('pages/admin.ejs');
+
+    },
+
+};
